a2f9ecbae9df687b6bd07886d5598326cc012fdd
Gabriel Oliveira <gabrieloliveradesouza9@gmail.com>
2019-08-23 18:10:39 -0300

    Created class

--- src/Log.php
+++ src/Log.php
@@ -4,16 +4,35 @@ namespace Log;
 
 use Log\Interfaces\LogInterface;
 
-class Log implements LogInterface{
+class Log extends FileHandling implements LogInterface{
 
-    public function setMessageError($msg, $file, $line,$code = null)
+    public function __construct($filePath)
     {
-
+        $this->setFilePath($filePath);
     }
 
-    public function setWarningMessage($msg, $file, $line, $code = null)
+    public function setMessageError($msg, $file, $line)
     {
+        return $this->setDefaultLogMessages("Error", $msg, $file, $line);   
+    }
+
+    public function setWarningMessage($msg, $file, $line)
+    {
+        return $this->setDefaultLogMessages("Warning", $msg, $file, $line);   
+    }
+
+    private function setDefaultLogMessages($messageType, $msg, $file, $line)
+    {
+        $writingResult = $this->LogFileWriting(
+            $messageType . " log on file ". $file .": at line ". $line .": - ".date("Y/m/d H:i:s")."\n" .$messageType ." Message: ". $msg.
+            "\n =======================================\n"
+            );
         
+        if(!$writingResult){
+            throw new \Exception("Error not defined while writing to log file.", 1);   
+        }
+
+        return $writingResult;
     }
 
 }
