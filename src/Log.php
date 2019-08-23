b2b1901e8e3b8ac270cbc240d2e8e67b06640074
Gabriel Oliveira <gabrieloliveradesouza9@gmail.com>
2019-08-23 18:57:05 -0300

    Documented Code

--- src/Log.php
+++ src/Log.php
@@ -6,21 +6,51 @@ use Log\Interfaces\LogInterface;
 
 class Log extends FileHandling implements LogInterface{
 
+    /**
+     * Set file path in function setFilePath().
+     *
+     * @param string $filePath
+     */
     public function __construct($filePath)
     {
         $this->setFilePath($filePath);
     }
 
+    /**
+     * Write  Error Message to log file.
+     *
+     * @param string $msg
+     * @param string $file
+     * @param int $line
+     * @return bool
+     */
     public function setMessageError($msg, $file, $line)
     {
         return $this->setDefaultLogMessages("Error", $msg, $file, $line);   
     }
 
+    /**
+     * Write  Warning Message to log file.
+     *
+     * @param string $msg
+     * @param string $file
+     * @param int $line
+     * @return bool
+     */
     public function setWarningMessage($msg, $file, $line)
     {
         return $this->setDefaultLogMessages("Warning", $msg, $file, $line);   
     }
 
+    /**
+     * Generate message format to be written;
+     *
+     * @param string $messageType
+     * @param string $msg
+     * @param string $file
+     * @param int $line
+     * @return bool
+     */
     private function setDefaultLogMessages($messageType, $msg, $file, $line)
     {
         $writingResult = $this->LogFileWriting(
